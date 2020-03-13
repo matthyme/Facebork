3d4f18e46cff9c37d5827fa5607f7e9190655752
matthyme <matthyme@gmail.com>
2020-03-13 01:25:18 -0500

    added bootstrap to landing, added eye toggle for pw peek

--- script/publish.sh
+++ script/publish.sh
@@ -7,4 +7,4 @@ mkdir "$SSH_PATH"
 echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
-sh -c "rsync -avzr --delete --exclude .git --exclude .github --exclude script -e 'ssh -p $CONNECTION_PORT -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/ $CONNECTION_STRING:$REMOTE_PATH"
+sh -c "rsync -avzr --delete --exclude .git --exclude .github --exclude script --exclude Server -e 'ssh -p $CONNECTION_PORT -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/ $CONNECTION_STRING:$REMOTE_PATH"
